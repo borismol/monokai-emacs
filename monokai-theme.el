b08637499bfd8133ca3d319dcd96b3a9e1613976
Boris Molodenkov <boris.molodenkov@gmail.com>
2025-01-24 21:47:35 +0300

    Change background to dark grey

--- monokai-theme.el
+++ monokai-theme.el
@@ -150,7 +150,7 @@ Also affects 'linum-mode' background."
   :type 'string
   :group 'monokai)
 
-(defcustom monokai-background "#272822"
+(defcustom monokai-background "#242728" ;; "#272822"
   "Adaptive colors - background"
   :type 'string
   :group 'monokai)
